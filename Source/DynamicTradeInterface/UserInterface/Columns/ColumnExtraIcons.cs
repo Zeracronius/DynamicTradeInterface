ca637c14fc98a7ba11f4dd2bb0c1966c0d130e8c
Zeracronius <zeracronius@outlook.dk>
2023-08-13 14:21:37 +0200

    Added xenotype indicator

--- Source/DynamicTradeInterface/UserInterface/Columns/ColumnExtraIcons.cs
+++ Source/DynamicTradeInterface/UserInterface/Columns/ColumnExtraIcons.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using Verse;
 
 namespace DynamicTradeInterface.UserInterface.Columns
 {
@@ -12,8 +13,29 @@ namespace DynamicTradeInterface.UserInterface.Columns
 	{
 		public static void Draw(ref Rect rect, Tradeable row, Transactor transactor, ref bool refresh)
 		{
-			float width = 0f;
+			float width = rect.x;
 			TransferableUIUtility.DoExtraIcons(row, rect, ref width);
+
+			if (ModsConfig.IdeologyActive)
+			{
+				if (row.IsThing)
+				{
+					Thing thing = row.AnyThing;
+					if (thing is Pawn pawn)
+					{
+						XenotypeDef? xeno = pawn.genes?.Xenotype;
+						if (xeno != null && xeno != XenotypeDefOf.Baseliner)
+						{
+							Rect xenoIconRect = new Rect(width, rect.y, 24, 24);
+							Widgets.DrawTextureFitted(xenoIconRect, xeno.Icon, 1);
+							if (Mouse.IsOver(xenoIconRect))
+								TooltipHandler.TipRegion(xenoIconRect, xeno.LabelCap);
+						}
+					}
+				}
+
+				//TODO Add icon for marking genepack as known or not.
+			}
 		}
 	}
 }
