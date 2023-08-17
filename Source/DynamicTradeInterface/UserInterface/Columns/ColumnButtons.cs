cb7af2b57f383171c1059b19258bdb3512d9e8ab
Zeracronius <zeracronius@outlook.dk>
2023-08-17 20:31:45 +0200

    Fixed recent cache optimizations breaking gifting mode.

--- Source/DynamicTradeInterface/UserInterface/Columns/ColumnButtons.cs
+++ Source/DynamicTradeInterface/UserInterface/Columns/ColumnButtons.cs
@@ -12,7 +12,6 @@ using Verse.Sound;
 
 namespace DynamicTradeInterface.UserInterface.Columns
 {
-	[HotSwappable]
 	internal static class ColumnButtons
 	{
 		private static Dictionary<Tradeable, (bool, bool, int, int)> _editableCache = new Dictionary<Tradeable, (bool, bool, int, int)>();
