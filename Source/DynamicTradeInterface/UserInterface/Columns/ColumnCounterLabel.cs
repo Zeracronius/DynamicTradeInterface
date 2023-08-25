6c2909f96dc5657fc424b9f132fef4848e7d8ca4
Zeracronius <zeracronius@outlook.dk>
2023-08-25 19:47:31 +0200

    Changed counter column to sort Buying => Selling => None.

--- Source/DynamicTradeInterface/UserInterface/Columns/ColumnCounterLabel.cs
+++ Source/DynamicTradeInterface/UserInterface/Columns/ColumnCounterLabel.cs
@@ -1,4 +1,5 @@
 ﻿using DynamicTradeInterface.Attributes;
+using DynamicTradeInterface.UserInterface.Columns.ColumnCounterTypes;
 using RimWorld;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ using Verse;
 
 namespace DynamicTradeInterface.UserInterface.Columns
 {
+	[HotSwappable]
 	internal static class ColumnCounterLabel
 	{
 		private struct Cache
@@ -127,7 +129,7 @@ namespace DynamicTradeInterface.UserInterface.Columns
 
 		public static Func<Tradeable, IComparable> OrderbyValue(Transactor transactor)
 		{
-			return (Tradeable row) => row.CountToTransfer;
+			return (Tradeable row) => new CounterComparable(row.CountToTransfer);
 		}
 	}
 }
---
+++ Source/DynamicTradeInterface/UserInterface/Columns/ColumnCounterTypes/CounterComparable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicTradeInterface.UserInterface.Columns.ColumnCounterTypes
+{
+	/// <summary>
+	/// Wraps integer to sort positive at the top, then negatives, then 0.
+	/// Used for counter column to sort buying, selling, none.
+	/// </summary>
+	/// <seealso cref="System.IComparable" />
+	struct CounterComparable : IComparable
+	{
+		int _value;
+		public CounterComparable(int value)
+		{
+			_value = value;
+		}
+
+		// return +1 to consider this instance greater, -1 to consider instance this smaller
+		public int CompareTo(object other)
+		{
+			if (other is CounterComparable comparable == false)
+				return 1;
+
+			int otherValue = comparable._value;
+			if (_value == otherValue)
+				return 0;
+
+			// If that is 0 and this isn't, then consider this greater.
+			if (otherValue == 0)
+				return 1;
+
+			// If this is 0 and that isn't then consider this smaller
+			if (_value == 0)
+				return -1;
+
+			// This is buying
+			if (_value > 0)
+			{
+				// That is buying
+				if (otherValue > 0)
+					return _value > otherValue ? 1 : -1;
+
+				// IF this is buying and that isn't then consider this greater.
+				return 1;
+			}
+
+			// This is selling
+			if (_value < 0)
+			{
+				// That is selling
+				if (otherValue < 0)
+					return _value > otherValue ? -1 : 1;
+
+				// If this is selling and that isn't, then consider this smaller.
+				return -1;
+			}
+
+			return 1;
+		}
+	}
+}
