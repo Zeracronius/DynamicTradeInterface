fb40fb48c66f96c16e7c283e09f09e56fcafbd65
Zeracronius <zeracronius@outlook.dk>
2023-08-27 21:53:00 +0200

    Added ghost functionality to info buttons

--- Source/DynamicTradeInterface/UserInterface/Columns/ColumnInfo.cs
+++ Source/DynamicTradeInterface/UserInterface/Columns/ColumnInfo.cs
@@ -11,14 +11,36 @@ namespace DynamicTradeInterface.UserInterface.Columns
 {
 	internal static class ColumnInfo
 	{
+		private static Dictionary<Tradeable, (Thing, bool)> _editableCache = new Dictionary<Tradeable, (Thing, bool)>();
+
+		public static void PostOpen(IEnumerable<Tradeable> rows, Transactor transactor)
+		{
+			foreach (var row in rows)
+			{
+				if (row.IsThing)
+				{
+					Thing thing = row.AnyThing;
+					if (thing != null)
+					_editableCache[row] = (thing, Mod.DynamicTradeInterfaceMod.Settings.GhostButtons);
+				}
+			}
+		}
+
+
+		public static void PostClosed(IEnumerable<Tradeable> rows, Transactor transactor)
+		{
+			_editableCache.Clear();
+		}
+
 		public static void Draw(ref Rect rect, Tradeable row, Transactor transactor, ref bool refresh)
 		{
-			if (row.IsThing)
-			{
-				Thing thing = row.AnyThing;
-				if (thing != null)
-					Widgets.InfoCardButton(rect.x, rect.y, thing);
-			}
+			if (_editableCache.TryGetValue(row, out (Thing, bool) cached) == false)
+				return;
+
+			if (cached.Item2 == true && Mouse.IsOver(rect.ExpandedBy(rect.width * 2, rect.height * 2)) == false)
+				return;
+
+			Widgets.InfoCardButton(rect.x, rect.y, cached.Item1);
 		}
 	}
 }
